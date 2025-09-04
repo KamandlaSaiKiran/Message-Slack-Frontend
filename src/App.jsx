b78fb3d0f74f0c4b7dbfad4da75fc5833eeb040a
Sai Kiran <kamandlasaikiran@gmail.com>
2025-09-04 09:07:38 +0530

    Added shadcn for ui components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      {/* <h1 className="text-8xl font-bold">Tailwind Sai Kiran ✅</h1> */}
+     <div>
+      <Button>Click me</Button>
+    </div>
+    </div>
+  )
+}
+
+export default App
