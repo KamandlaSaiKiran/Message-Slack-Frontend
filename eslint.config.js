4975afcd44b661a0912c898dcdcdcb762cf8eee3
Sai Kiran <kamandlasaikiran@gmail.com>
2025-09-05 13:12:18 +0530

    Added eslint setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -24,6 +25,21 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
+    'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+       ...reactHooks.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'react/prop-types': 'off'
+      
   },
+}
 ])
