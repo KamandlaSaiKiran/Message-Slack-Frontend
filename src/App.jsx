a96be2f97b6bf2cd432305b67af354921e6c4635
Sai Kiran <kamandlasaikiran@gmail.com>
2025-09-06 11:57:51 +0530

    Added basic signup card ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { Route, Routes } from "react-router-dom"
+import {Auth} from '@/pages/Auth/Auth'
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      {/* <h1 className="text-8xl font-bold">Tailwind Sai Kiran ✅</h1> */}
-     <div>
-      <Button>Click me</Button>
-    </div>
-    </div>
+  <Routes>
+    <Route path="/auth" element={<Auth/>}></Route>
+  </Routes>
   )
 }
 
--- src/hooks/apis/auth/useSignup.js
+++ src/hooks/apis/auth/useSignup.js
@@ -5,10 +5,10 @@ export const useSignup = ()=>{
     const {isPending,isSuccess,error,mutate:signupMutation}=useMutation({
             mutationFn:signUpRequest,
             onSuccess: (data) => {
-            console.log('Scuccessfuilly signed up', data);
+            console.log('Successfuilly signed up', data);
         },
         onError: (error) => {
-            console.error('Failed to sign up', error);
+            console.log('Failed to sign up', error);
         }
         
     });
