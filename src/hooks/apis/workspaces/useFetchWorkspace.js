ae32ea015ce89a4654284384708d86c75b5d7546
Sai Kiran <kamandlasaikiran@gmail.com>
2025-09-14 20:46:34 +0530

    Added APIs and hooks for creating and fetching workspaces

---
+++ src/hooks/apis/workspaces/useFetchWorkspace.js
@@ -0,0 +1,20 @@
+import { fetchWorkspaceRequest } from "@/apis/auth/workspaces";
+import { useAuth } from "@/hooks/context/useAuth";
+import { useQuery } from "@tanstack/react-query";
+
+export const useFetchWorkspace = ()=>{
+    const {auth} = useAuth();
+
+    const {isFetching,isSuccess,error,data:workspaces}= useQuery({
+        queryFn: ()=>fetchWorkspaceRequest({token:auth?.token}),
+        queryKey: 'fetchWorkspaces',
+        staleTime: 30000
+    });
+
+    return{
+        isFetching,
+        isSuccess,
+        error,
+        workspaces
+    }
+}
